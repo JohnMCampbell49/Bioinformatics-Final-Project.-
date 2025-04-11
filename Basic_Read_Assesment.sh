273821482ab4de152e05b5ca7da32ebafc86dfd7
Eric Boheen <erb1125@ron.sr.unh.edu>
2025-04-11 17:34:18 -0400

    Basic_Read_Assesment.sh

---
+++ Basic_Read_Assesment.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+path='/home/users/erb1125/Final_Project/Reads/'
+for item in $path/*
+   do
+	echo $(basename $item)
+	read_number=$(zgrep -c "^@" $item)
+	echo Read number=$read_number
+	echo total base pairs=$(($read_number * 250 * 2))
+	total_bp=$(($read_number * 250 * 2))
+	echo coverage=$(($total_bp/7000000))
+done
+conda activate genomics
+for item in $path/*
+   do
+	fastqc -o /home/users/erb1125/Final_Project/Fastqc_outputs $item
+	multiqc /home/users/erb1125/Final_Project/Fastqc_outputs 
+done 
+ 
