1f1891589efe80b699704e9c7165e181eb51979b
Eric Boheen <erb1125@ron.sr.unh.edu>
2025-05-12 12:09:53 -0400

    added visualizer and scripts to make rerunning the genome faster

--- busco.sh
+++ busco.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 source activate genomics
-busco -i ~/Final_Project/$1 -m genome -o busco-results -l bacteria
-#input directory in Final_Project that has the contigs file and then enter the conig file for an argument (directory/contigs.file)
+busco -i $1 -m genome -o busco-results -l bacteria
+#input path to fasta file
 mkdir ~/Final_Project/Busco
 mv ./busco-results ~/Final_Project/Busco
+#note if busco has already been run you have have to manual move the result directory 
 date
